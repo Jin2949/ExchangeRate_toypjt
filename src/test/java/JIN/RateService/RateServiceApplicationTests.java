bfd868e841631eff928b08d3a0f34a4ceefa14df
jin <hyungjin2949@gmail.com>
2023-12-26 19:29:52 +0900

    test delete

--- src/test/java/JIN/RateService/RateServiceApplicationTests.java
+++ src/test/java/JIN/RateService/RateServiceApplicationTests.java
@@ -3,7 +3,6 @@ package JIN.RateService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RateServiceApplicationTests {
 
 	@Test
